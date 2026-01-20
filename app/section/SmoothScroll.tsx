95505c5b4d1b59baffacfd0e71fdc1f1de28ea44
Pasindu Malinda <pasindumalinda994a@gmail.com>
2026-01-20 10:56:00 +0530

    update vercel production

--- app/section/SmoothScroll.tsx
+++ app/section/SmoothScroll.tsx
@@ -12,7 +12,7 @@ export default function SmoothScroll({ children }: { children: React.ReactNode }
       gestureOrientation: 'vertical',
       smoothWheel: true,
       wheelMultiplier: 1,
-      smoothTouch: false,
+      syncTouch: false,
       touchMultiplier: 2,
       infinite: false,
     });
